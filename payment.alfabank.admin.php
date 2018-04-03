2aa5ef9d38b149a6302fd4504f4246e33ef42513
Gar|k <garik.djan@gmail.com>
2018-04-04 02:47:13 +0500

    add orderBundle

--- payment.alfabank.admin.php
+++ payment.alfabank.admin.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Настройки платежной системы Альфа-Банк для административного интерфейса
  * 
@@ -8,31 +9,57 @@
  * @license    http://www.diafan.ru/license.html
  * @copyright  Copyright (c) 2003-2016 OOO «Диафан» (http://www.diafan.ru/)
  */
-
-if (! defined('DIAFAN'))
-{
-	$path = __FILE__; $i = 0;
-	while(! file_exists($path.'/includes/404.php'))
-	{
-		if($i == 10) exit; $i++;
-		$path = dirname($path);
-	}
-	include $path.'/includes/404.php';
+if (!defined('DIAFAN')) {
+    $path = __FILE__;
+    $i = 0;
+    while (!file_exists($path . '/includes/404.php')) {
+        if ($i == 10)
+            exit;
+        $i++;
+        $path = dirname($path);
+    }
+    include $path . '/includes/404.php';
 }
 
-class Payment_alfabank_admin
-{
-	public $config;
+class Payment_alfabank_admin {
 
-	public function __construct()
-	{
-		$this->config = array(
-			"name" => 'Альфа-Банк',
-			"params" => array(
-				'userName' => 'Логин магазина',
-				'password' => 'Пароль магазина',
-				'test' => array('name' => 'Тестовый режим', 'type' => 'checkbox'),
-			)
-		);
-	}
+    public $config;
+
+    public function __construct() {
+        $this->config = array(
+            "name" => 'Альфа-Банк',
+            "params" => array(
+                'userName' => 'Логин магазина',
+                'password' => 'Пароль магазина',
+                'tax' => array(
+                    'type' => 'select',
+                    'name' => 'Настройки ставок НДС',
+                    'help' => 'Для магазинов с настройкой фискализации',
+                    'select' => array(
+                        '0' => 'НДС не облагается',
+                        '1' => 'ставка НДС 0%',
+                        '2' => 'ставка НДС 10%',
+                        '3' => 'ставка НДС 18%',
+                        '4' => 'ставка НДС расч. 10/110',
+                        '5' => 'ставка НДС расч. 18/118'
+                    ),
+                ),
+                'taxationSystem' => array(
+                    'type' => 'select',
+                    'name' => 'Система налогообложения',
+                    'help' => 'Для магазинов с настройкой фискализации',
+                    'select' => array(
+                        '0' => 'Общая, ОСН',
+                        '1' => 'Упрощенная доход, УСН доход',
+                        '2' => 'Упрощенная доход минус расход, УСН доход - расход',
+                        '3' => 'Единый налог на вмененный доход, ЕНВД',
+                        '4' => 'Единый сельскохозяйственный налог, ЕСН',
+                        '5' => 'Патентная система налогообложения, Патент'
+                    )
+                ),
+                'test' => array('name' => 'Тестовый режим', 'type' => 'checkbox'),
+            )
+        );
+    }
+
 }
--- payment.alfabank.model.php
+++ payment.alfabank.model.php
@@ -24,6 +24,64 @@ if (!defined('DIAFAN')) {
 class Payment_alfabank_model extends Diafan {
 
     /**
+     * Конвертирует в float результат работы функции number_format
+     * @param string $number
+     * @return float
+     */
+    private function parse_number($number) {
+        $dec_point = $this->diafan->configmodules("format_price_2", "shop");
+        return floatval(str_replace($dec_point, '.', preg_replace('/[^\d' . preg_quote($dec_point) . ']/', '', $number)));
+    }
+
+    private function getOrderBundle($order_id, $taxType = 0) {
+        $info = $this->diafan->_shop->order_get($order_id);
+        
+        // цены передаются без учета скидки общей...
+        $discount = 0;
+        if(!empty($info['discount_summ'])) {
+            $discount = $this->parse_number($info['discount_summ'])/intval($info['count']);
+        }
+
+        $result = array(
+            'cartItems' => array(
+                'items' => array()
+            )
+        );
+
+        foreach ($info['rows'] as $i => $row) {
+            $item = array();
+            $item['positionId'] = $i;
+            $item['name'] = $row['name'];
+            $item['quantity'] = array(
+                'value' => intval($row['count']),
+                'measure' => (!empty($row['measure_unit']) ? $row['measure_unit'] : 'шт')
+            );
+            $item['itemCode'] = $row['id'];
+            $item['itemPrice'] = ($this->parse_number($row['price']) - $discount) * 100;
+            $item['tax'] = array('taxType' => intval($taxType));
+
+            $result['cartItems']['items'][] = $item;
+        }
+        
+        if(!empty($info['delivery'])) {
+            $item = array();
+            $item['positionId'] = count($info['rows']);
+            $item['name']=$info['delivery']['name'];
+            $item['quantity'] = array(
+                'value' => 1,
+                'measure' => 'шт'
+            );
+            $item['itemCode'] = $info['delivery_id'];
+            $item['itemPrice'] = $this->parse_number($info['delivery']['summ']) * 100;
+            $item['tax'] = array('taxType' => intval($taxType));
+            
+            $result['cartItems']['items'][] = $item;
+        }
+
+        return json_encode($result);
+    }
+
+    /**
      * Формирует данные для формы платежной системы "Альфа-Банк"
      * 
      * @param array $params настройки платежной системы
@@ -36,8 +94,7 @@ class Payment_alfabank_model extends Diafan {
 
         try {
             $url = (!empty($params['test']) ? 'https://web.rbsuat.com/ab/rest/' : 'https://pay.alfabank.ru/payment/rest/');
-            
-      
+
             // регистрация заказа
             $http = DHttpRequest::post($url . 'register.do')->form(
                             array(
@@ -47,7 +104,10 @@ class Payment_alfabank_model extends Diafan {
                                 'amount' => intval(floatval($pay['summ']) * 100),
                                 'returnUrl' => BASE_PATH . 'payment/get/alfabank/success/?' . rand(1111, 9999),
                                 'failUrl' => BASE_PATH . 'payment/get/alfabank/fail/?' . rand(1111, 9999),
-                                'description' => $pay['desc'])
+                                'description' => $pay['desc'],
+                                'orderBundle' => $this->getOrderBundle($pay['element_id'], $params['tax']),
+                                'taxSystem' => $params['taxationSystem']
+                            )
                     )->acceptJson();
 
             $json = $http->ok() ? json_decode($http->body(), true) : null;
